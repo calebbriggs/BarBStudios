ec940b3c41ccb2a6a11e894edd80c4000b3aec6b
Caleb Briggs <caleb@ninecollective.com>
2013-09-24 10:32:45 -0500

    taking out presentation

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ function compile(str, path) {
     .use(nib())
 };
 app.configure(function () {
-		
         app.set('views', __dirname+ '/views');
         app.set('view engine', 'ejs');        
 		app.use(express.bodyParser());
